d4483649a68ffc4e7117d213193a4877db4149b0
Michał Kałużny <michalkaluzny@gmail.com>
2014-10-15 19:58:15 +0200

    Added agenda, sticky nav menu, iWatch support and many more

--- public/js/app.js
+++ public/js/app.js
@@ -5,9 +5,26 @@ $('.sticky a.hamburger').click(function (e) {
 
 $(document).scroll(function () {
     var y = $(this).scrollTop();
-    if (y > 580) {
+    if (y > 539) {
         $('.sticky').addClass('active');
     } else {
         $('.sticky').removeClass('active');
+    }
+});
+
+$('nav').onePageNav({
+    currentClass: 'selected',
+    changeHash: false,
+    scrollSpeed: 750,
+    scrollThreshold: 0.5,
+    filter: '',
+    easing: 'swing',
+    begin: function() {
+        //I get fired when the animation is starting
+    },
+    end: function() {
+        //I get fired when the animation is ending
+    },
+    scrollChange: function($currentListItem) {
     }
 });
